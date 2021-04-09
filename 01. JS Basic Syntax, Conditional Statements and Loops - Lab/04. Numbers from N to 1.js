e4d8bcc191cb93ffd9bcafc5f43597150d9cce43
Veronika Varcheva <73795486+veronikavarcheva@users.noreply.github.com>
2021-04-09 11:44:29 +0300

    delete files

--- 01. JS Basic Syntax, Conditional Statements and Loops - Lab/04. Numbers from N to 1.js
+++
@@ -1,9 +0,0 @@
-function solve(N) {
-    while (N >= 1) {
-        console.log(N);
-        N--;        
-    }
-}
-
-solve(5);
-solve(3);
--- 01. JS Basic Syntax, Conditional Statements and Loops - Lab/1.Multiply Number by 2.js
+++
@@ -1,7 +0,0 @@
-function multipleResult (number) {
-    console.log(number*2);    
-}
-
-multipleResult (2);
-multipleResult (5);
-multipleResult (20);
