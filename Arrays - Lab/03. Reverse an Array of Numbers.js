2ff6225b6d60dc91194d49d7be85c34257030be6
Veronika Varcheva <73795486+veronikavarcheva@users.noreply.github.com>
2021-04-13 16:53:34 +0300

    Add files

--- Arrays - Lab/03. Reverse an Array of Numbers.js
+++ Arrays - Lab/03. Reverse an Array of Numbers.js
@@ -7,13 +7,13 @@ for (let i = 0; i < n; i++){
     
 }
 let output = '';
-for(let j = newArr.length-1; j >= 0; j--){
+for (let j = newArr.length-1; j >= 0; j--){
     output += `${Number(newArr[j])} `;
 }
-console.log(output);
+console.log (output);
 
 }
 
-solve(3, [10, 20, 30, 40, 50]);
-solve(4, [-1, 20, 99, 5]);
-solve(2, [66, 43, 75, 89, 47]);
+solve (3, [10, 20, 30, 40, 50]);
+solve (4, [-1, 20, 99, 5]);
+solve (2, [66, 43, 75, 89, 47]);
